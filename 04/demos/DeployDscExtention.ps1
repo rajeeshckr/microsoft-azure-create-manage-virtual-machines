c8762f8cfa53659173cf0d7a7474bc3bdefbf6d4
Rajeesh Kuttan <rkuttan@flexera.com>
2018-09-05 09:42:59 +1000

    first commit

---
+++ 04/demos/DeployDscExtention.ps1
@@ -0,0 +1,34 @@
+﻿<#
+
+Author: Mike Pfeiffer
+
+Sample DSC Extention Script
+
+Disclaimers
+
+!!!!!!!!!!
+This script is provided as an example and is not directly intended to be run as-is.
+!!!!!!!!!!
+
+This example code is provided without copyright and AS IS.  It is free for you to use and modify.
+Note: These demos should not be run as a script. These are the commands that I use in the 
+demonstrations and would need to be modified for your environment.
+
+#>
+
+$resourceGroup = 'Servers'
+$location = 'westus2'
+$vmName = 'Srv1'
+$storageName = '41480434'
+
+Publish-AzureRmVMDscConfiguration -ConfigurationPath .\WindowsWebServer.ps1 `
+-ResourceGroupName $resourceGroup `
+-StorageAccountName $storageName -force
+
+Set-AzureRmVmDscExtension -Version 2.21 `
+-ResourceGroupName $resourceGroup `
+-VMName $vmName `
+-ArchiveStorageAccountName $storageName `
+-ArchiveBlobName WindowsWebServer.ps1.zip `
+-AutoUpdate:$true `
+-ConfigurationName IIS
---
+++ 09/demos/Install-WebServer.ps1
@@ -0,0 +1,3 @@
+﻿Install-WindowsFeature web-server
+
+New-Item -ItemType file -Name index.aspx -Path c:\inetpub\wwwroot\ -Value "<h1>WEB1</h1>" -Force
